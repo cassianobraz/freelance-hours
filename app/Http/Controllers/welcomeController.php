7731f09b13283e44f417964d77336eace3631ceb
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-10-11 13:00:42 -0400

    :sparkles: feat: update in schemas and seeds

--- app/Http/Controllers/welcomeController.php
+++ app/Http/Controllers/welcomeController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class welcomeController extends Controller
+class WelcomeController extends Controller
 {
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        //
+        return view('welcome');
     }
 }
